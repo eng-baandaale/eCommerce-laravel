637535fad715a4da358ef1a7c410b9c0cf249b41
eng-baandaale <baandaale@gmail.com>
2023-01-02 22:12:41 +0300

    header,footer and login page done:

---
+++ resources/views/header.blade.php
@@ -0,0 +1,36 @@
+<nav class="navbar navbar-expand-lg bg-primary">
+<div class="container-fluid" >
+    <a class="navbar-brand" style="color:white;" href="#">E-SOM</a>
+    <button class="navbar-toggler"  type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarScroll">
+      <ul class="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll" style="--bs-scroll-height: 100px;">
+        <li class="nav-item" style="color:white;">
+          <a class="nav-link active" aria-current="page" href="#" style="color:white;">Home</a>
+        </li>
+        <li class="nav-item" style="color:white;">
+          <a class="nav-link" href="#" style="color:white;">Link</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" style="color:white;" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Link
+          </a>
+          <ul class="dropdown-menu" style="color:white;">
+            <li><a class="dropdown-item" href="#">Action</a></li>
+            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><hr class="dropdown-divider"></li>
+            <li><a class="dropdown-item"  href="#">Something else here</a></li>
+          </ul>
+        </li>
+        <li class="nav-item" style="color:white;">
+          <a class="nav-link disabled" style="color:lightgrey;">Link</a>
+        </li>
+      </ul>
+      <form class="d-flex" role="search" style="color:white;">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-danger" style="color:white;" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
